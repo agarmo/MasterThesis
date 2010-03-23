8ece2221398bfc9d349462671c426fb1c63b2b32
Anders Moen Garmo <anderga@caracal.stud.ntnu.no.stud.ntnu.no>
2010-03-23 14:56:19 +0100

    Some genral comments

--- project/representation/node.m
+++ project/representation/node.m
@@ -2,7 +2,18 @@
 %% representation. 
 % The plan is to make this class a super class and let the different types
 % of nodes inherit form this one.
-
+% Description of the properties:
+%           type: the type of the node.
+%           number: is the number in the node, usually describing the
+%                   number in which it is discovered
+%           prev_node: is the link to the previous visited node.
+%           discovered: is the timestamp when the node was recognized
+%           diameter: the estimated diameter of the pipe
+%           dist_prev_node: is the estimated distance to the previous node.
+%           anomalies: A list of anomalies discovered between this node and
+%                      the previous one,
+% List of methods:
+%           
 classdef node
     properties
         type = 'Super Node';
