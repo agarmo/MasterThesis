15db468fd13353d5cf2861cea0a52e3929818c85
Anders Garmo <agarmo@gmail.com>
2010-03-25 16:10:59 +0100

    Trying new branch. Fixing world representation

--- project/representation/t_junction.m
+++ project/representation/t_junction.m
@@ -1,10 +1,12 @@
 %% Class defs of T-junction
 
 classdef t_junction < node
-    properties
-        type = 'T-Junction';        
+    properties    
     end
     methods
-        
+        function [t] = setType(obj, nodetype)
+            obj.type = nodetype;
+            t = obj;
+        end
     end
 end
