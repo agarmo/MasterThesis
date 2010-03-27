5dbde3bb41ae694951404746ec1c99e0114e6de3
Anders Garmo <agarmo@gmail.com>
2010-03-27 16:05:50 +0100

    Added the super class world, which is going to hold the world.

--- project/representation/draw_graph.m
+++ project/representation/draw_graph.m
@@ -1,5 +1,6 @@
 %% Function to draw the graphs from all the discovered nodes. 
-
+% This function need to find all the discovered nodes, and place them 
+% with connections to each other. This must be 
 
 function [] = draw_graph()
 
---
+++ project/representation/world.m
@@ -0,0 +1,24 @@
+%% World class, containing all the nodes. 
+% This is the container class for all the nodes stored in the world
+% 
+
+classdef world
+    
+    properties %(SetAccess = private) % ?
+        nodes; % the nodes that are discovered. 
+    end
+    
+    methods
+        %%  Constuctor
+        function [object] = world(object)
+            
+        end
+        
+        %% Othre functions
+        
+        % add node function for adding nodes to the world
+        function [object] = addNode(node, x, y)
+            
+        end
+    end
+end
