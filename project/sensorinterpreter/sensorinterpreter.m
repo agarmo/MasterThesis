08ecd10ccfcb2ac914e4bccfc92fef6e98ef1299
Anders Garmo <agarmo@gmail.com>
2010-03-31 13:13:26 +0200

    Started on the sensor interpreter class.

---
+++ project/sensorinterpreter/sensorinterpreter.m
@@ -0,0 +1,26 @@
+%% Sensor Interpreter
+%
+% This is the class object which is resposible for recognizing the sensor
+% output. 
+% Output form this class is:    - Node Type
+%                               - Position and Orientation
+%                               - Distance traveled
+%                               - Velocity
+%                               - Distance to walls (?)
+%                               - Register anomalies and irregualrities
+
+
+classdef sensorinterpreter 
+   
+    properties
+        
+        
+    end
+    
+    methods
+        
+        
+        
+    end
+    
+end
