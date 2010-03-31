ef7e94d0682f241993c09bd4bfb66914d76cda1e
Anders Garmo <agarmo@gmail.com>
2010-03-31 13:43:57 +0200

    Made some of the framwork on the sensorinterpreter class

--- project/sensorinterpreter/sensorinterpreter.m
+++ project/sensorinterpreter/sensorinterpreter.m
@@ -12,12 +12,57 @@
 
 classdef sensorinterpreter 
    
-    properties
+    properties(GetAccess = public, SetAccess = protected)
+        %% The sensor readings.
+        LR_data;
+        ToF_data;
+        Stereo_data;
+    
+        %% Output data Whenever recognized
+        recognized_node_struct; % A struct containing the data needed for the world object to draw the node.
+        registered_anomalies_struct; % A struct containing details about the detected anomaly.
+        
+        %% Continous output data
+        velocities; % size 3, 1 
+        dist_to_walls; % size 2, 1 
+    end
+    
+    properties(GetAccess = private, SetAccess = private)
+        %% Internal properties
         
         
     end
     
     methods
+        %% Constructor
+        
+        function object = sensorinterpreter(args)
+           
+            if nargin < 1 % default constuctor
+                object.velocities = zeros(3,1);
+                object.dist_to_walls = zeros(2,1);
+                
+                
+            elseif nargin == 2
+                
+                
+            end
+            
+            
+        end
+        
+        
+        
+        %% Internal Calculation Function
+        
+        
+        
+        %% Output function
+        
+        
+        
+        %% Other functions
+        
         
         
         
