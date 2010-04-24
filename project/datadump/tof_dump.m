b6bc47a6e2cfaf6d777742994dce62a6ed230efd
Anders Moen Garmo <anderga@lynx.stud.ntnu.no.stud.ntnu.no>
2010-04-24 22:23:47 +0200

    The dump scripts are ready.

--- project/datadump/tof_dump.m
+++ project/datadump/tof_dump.m
@@ -1,39 +1,33 @@
 %% ToF camera dump script
 
-function [] = tof_dump(filename)
+function [xall, yall, zall] = tof_dump(filename, iterations)
 
     [dev] = sr_open(); % open the connection
-
-    sr_acquire(dev); % start the acquiring process
 
     xall = [];
     yall = [];
     zall = [];
+    
+    tic;
+    for i = 0:iterations; % the main loop
 
-    for i = 0:300; % the main loop
-
+        sr_acquire(dev); % start the acquiring process
+        
         [res, x, y, z] = sr_coordtrf(dev);
 
         %stor the coordinates in the overall array
-        xall = [xall, x];
-        yall = [yall, y];
-        zall = [zall, z];
-
-
+        xall = [xall; x];
+        yall = [yall; y];
+        zall = [zall; z];
 
     end
-
     sr_close(dev);
+    toc
     
-    %open the file
-    
-    file = fopen(filename, 'w');
-    
-    fprintf(file, 'Tof datadump\n\n');
-    
-    
-    
-        
-    fclose(filename);
+    temp = [xall; yall; zall];
 
+    csvwrite(filename, temp );
+    
+    
+    
 end
