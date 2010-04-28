2aa1bfb8c20b2f33dda663a8cdf325e900bd98e8
Anders Moen Garmo <anderga@lynx.stud.ntnu.no.stud.ntnu.no>
2010-04-28 19:14:04 +0200

    The cone fit or cylinder fit is working. The model is showed in the surface plot.

--- project/datadump/create_pos_vec.m
+++ project/datadump/create_pos_vec.m
@@ -1,4 +1,40 @@
+clear all;close all;
 
+temp = csvread('C:\Documents and Settings\anderga\My Documents\MATLAB\tof-pos1-control.txt');
+
+i = size(temp);
+ 
+xall = temp(1:i/3,:);
+yall = temp(i/3+1:2/3*i,:);
+zall = temp(2/3*i+1:end,:);
+
+clear temp;
+
+n = 4;
+
+x = xall(176*n+1:176*(n+1), :);
+y = yall(176*n+1:176*(n+1), :);
+z = zall(176*n+1:176*(n+1), :);
+
+pos = [];
+threshold = 0;
+
+for i = 1:176
+    for j = 1:176   
+        if j > 144
+            pos(i, j, :) = zeros(1,1,3);
+        else
+            if z(i,j) <= threshold
+                x(i,j) = 0;
+                y(i,j) = 0;
+                z(i,j) = 0;
+            end
+            pos(i,j,1) = x(i,j);
+            pos(i,j,2) = y(i,j);
+            pos(i,j,3) = z(i,j);
+        end
+    end
+end
 
 pos_vec = [];
 
@@ -20,8 +56,8 @@ s = 1;
 
 param = [x0, y0, alpha, beta, s];
 
-[x0n, an, rn, d, sigmah, conv, Vx0n, Van, urn, GNlog, a, R0, R] = ...
-    lscylinder(temp, [0, 0,0]', [0,0,1]', 0.25,1,1);
+[x0n, an, phin, rn, d, sigmah, conv, Vx0n, Van, uphin, urn, ... 
+GNlog, a, R0, R] = lscone(temp, [0, 0,0.5]', [0,0,1]', 5*pi/180, 0.125, 0.1, 0.1);
 
 
 % [f] = fgcylinder(param, pos_vec);
@@ -31,13 +67,29 @@ param = [x0, y0, alpha, beta, s];
 %% start constructing the cylinder from the given parameters.
 
 
-x = -1:0.01:1;
-y = -1:0.01:1;
-z = -1:0.01:1;
+[X, Y, Z] = cylinder([rn*1, rn*sin(phin)], 125);
 
-xa = (x./an(1)).^2 -rn^2;
-yn = (y./an(2)).^2 -rn^2;
-zn = (z./an(3)).^2 -rn^2;
+X = X-x0n(1);
+Y = Y-x0n(2);
+Z = (Z+x0n(3))./(1+x0n(3));
 
-plot3(xa,yn,zn)
+Rx = rotox(acos(an(2)));
+Ry = rotoy(acos(an(3)));
+Rz = rotoz(acos(an(1)));
+U = rot3z(an);
+for i = 1:126
+    for j =1:2
+        X(j,i) = U(1,:)*[X(j,i);Y(j,i);Z(j,i)];
+        Y(j,i) = U(2,:)*[X(j,i);Y(j,i);Z(j,i)];
+        Z(j,i) = U(3,:)*[X(j,i);Y(j,i);Z(j,i)];
+    end
+end
+
+start = 1;
+surfl(pos(:,:,3), pos(:,:,1), pos(:,:,2));
+hold on
+surf(Z(start:end,:), X(start:end,:), Y(start:end,:));
+axis equal
+hold off
+
 
