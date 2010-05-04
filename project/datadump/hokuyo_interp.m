1f4e4ba1786cea091000612ca67883467d54883f
Anders Moen Garmo <anderga@lynx.stud.ntnu.no.stud.ntnu.no>
2010-05-04 15:09:16 +0200

    The line fit is working adequatly for both x and y

--- project/datadump/hokuyo_interp.m
+++ project/datadump/hokuyo_interp.m
@@ -1,7 +1,7 @@
 %% Script interpreting urg-range data.
 close all; clear all;
 
-urg = csvread('C:\Documents and Settings\anderga\My Documents\MATLAB\urg-pos1-control.txt');
+urg = csvread('C:\Documents and Settings\anderga\My Documents\MATLAB\urg-pos1-irregularB.txt');
 ranges = hokuyo_parse_range(urg,1,size(urg)); % parses the ranges and angles in rad and meters
 
 %% start interpreting the data.
@@ -23,10 +23,11 @@ sorted = sortrows([urgx', urgy'], -1);
 temp = sorted;
 temp(~any(sorted,2),:)=[]; %% remove trivial points (0,0)
 
-histrange = (-1.6:0.05:0.7)';
+histrangey = (-1.6:0.05:0.7)';
+histrange = (-1.6:0.1:0.7)';
 
 [nx, binx] = hist(temp(:,1), histrange);
-[ny, biny] = hist(temp(:,2), histrange);
+[ny, biny] = hist(temp(:,2), histrangey);
 
 %pick out interesting poitns.
 
@@ -39,12 +40,20 @@ normd_urg =[];
 x_urg = [];
 y_urg = [];
 
+x_urgx = [];
+y_urgx = [];
+
+x0_urgx = [];
+a_urgx = [];
+d_urgx = [];
+normd_urgx =[];
+
 for k = 1:47
         
-    threshold = histrange(k);
-    
+    threshold = histrangey(k);
+    % horizontal lines. from the parallell to the y-axis
     data = [];
-    if ny(k) > 20
+    if ny(k) > 30
         for i = 1:size(temp(:,2))
             if (temp(i,2) > threshold-0.05) && (temp(i,2) < threshold+0.05)
                 data = [data; temp(i,:)];
@@ -52,7 +61,7 @@ for k = 1:47
         end
     end
     
-    %look for different shapes, arcs, lines,
+    %look for different shapes, arcs, lines, parallell to the y-axis
     if (~isempty(data)) && (size(data, 1) > 2)
         [x0_urgt, a_urgt, d_urgt, normd_urgt] = ls2dline(data);
         x0_urg = [x0_urg; x0_urgt'];
@@ -62,17 +71,56 @@ for k = 1:47
         
         %calculate the line
         
-        %scaling is difficutl!!! 
-        x_urgt_s = (x0_urgt(1) + ((a_urgt(1)*0)).*min(data(:,1));
-        y_urgt_s = (x0_urgt(2) + ((a_urgt(2)*0)).*min(data(:,2));
+        %scaling
+        startline = -x0_urgt(1)+ min(data(:,1));        
+        t = sqrt((max(data(:,1))-min(data(:,1)))^2 + (max(data(:,2))-min(data(:,2)))^2) ;
         
-        x_urgt_l = (x0_urgt(1) + (a_urgt(1)*1)).*max(data(:,1));
-        y_urgt_l = (x0_urgt(2) + (a_urgt(2)*1)).*max(data(:,2));
+        x_urgt_s = (x0_urgt(1) + ((a_urgt(1)*startline)));
+        y_urgt_s = (x0_urgt(2) + ((a_urgt(2)*startline)));
+        
+        x_urgt_l = (x0_urgt(1) + (a_urgt(1)*t));
+        y_urgt_l = (x0_urgt(2) + (a_urgt(2)*t));
         
         x_urg = [x_urg; [x_urgt_s, x_urgt_l]];
         y_urg = [y_urg; [y_urgt_s, y_urgt_l]];
     end
+end
 
+% look along the x-axis
+for k = 1:size(histrange)
+    threshold = histrange(k);
+    datax = [];
+    if nx(k) > 30
+        for i = 1:size(temp(:,2))
+            if (temp(i,1) > threshold-0.1) && (temp(i,1) < threshold+0.1)
+                datax = [datax; temp(i,:)];
+            end
+        end
+    end    
+    if (~isempty(datax)) && (size(datax, 1) > 2)
+        [x0_urgtx, a_urgtx, d_urgtx, normd_urgtx] = ls2dline(datax);
+        x0_urgx = [x0_urgx; x0_urgtx'];
+        a_urgx = [a_urgx; a_urgtx'];
+        %d_urg = [d_urg; d_urgt'];
+        normd_urgx =[normd_urgx; normd_urgtx];
+        
+        %calculate the line
+        
+        %scaling
+        startline = -x0_urgtx(2)+ min(datax(:,2));        
+        t = sqrt((max(datax(:,1))-min(datax(:,1)))^2 + (max(datax(:,2))-min(datax(:,2)))^2) ;
+        
+        x_urgt_s = (x0_urgtx(1) + ((a_urgtx(1)*startline)));
+        y_urgt_s = (x0_urgtx(2) + ((a_urgtx(2)*startline)));
+        
+        x_urgt_l = (x0_urgtx(1) + (a_urgtx(1)*t));
+        y_urgt_l = (x0_urgtx(2) + (a_urgtx(2)*t));
+        
+        x_urgx = [x_urgx; [x_urgt_s, x_urgt_l]];
+        y_urgx = [y_urgx; [y_urgt_s, y_urgt_l]];
+    end
+    
+    
 end
 
 
@@ -85,15 +133,26 @@ title('Plot of the URG Laser Range finder');
 
 figure;
 subplot(2, 1, 1);
-plot(sorted(:,1), sorted(:,2), 'r.');
+plot(sorted(:,1), sorted(:,2), 'b.');
 hold on;
 % plot(urgx, urgy, '.');
 grid on;
 xlabel('Depth into the pipe Z-axis [m]');
 ylabel('X-axis relative to ToF-camera [m]');
 title('URG Laser Range Finder');
+
 for i = 1:size(x_urg,1)
-    line([x_urg(i,1);x_urg(i,2)],[y_urg(i,1);y_urg(i,2)]);
+    line([x_urg(i,1);x_urg(i,2)],[y_urg(i,1);y_urg(i,2)], 'Color', 'magenta', ...
+        'LineWidth', 1.5);
+    %plot centroid
+    plot(x0_urg(i,1), x0_urg(i,2), 'k*');
+end
+
+for i = 1:size(x_urgx,1)
+    line([x_urgx(i,1);x_urgx(i,2)],[y_urgx(i,1);y_urgx(i,2)], 'Color', 'red', ...
+         'LineWidth', 1.5);
+    %plot centroid
+    plot(x0_urgx(i,1), x0_urgx(i,2), 'g*');
 end
 hold off;
 
@@ -108,5 +167,5 @@ hist(temp(:,1), histrange);
 title('Histogram of x-coordinates');
 
 subplot(1, 2, 2);
-hist(temp(:,2), histrange);
+hist(temp(:,2), histrangey);
 title('Histogram of y-coordinates');
