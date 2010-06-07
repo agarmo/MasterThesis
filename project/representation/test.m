7288fe60ff9a9808b132bf8df56ff54fa7396081
Anders Moen Garmo <anderga@lynx.stud.ntnu.no.stud.ntnu.no>
2010-06-07 15:14:22 +0200

    First commit today. where is the effectivity

--- project/representation/test.m
+++ project/representation/test.m
@@ -3,17 +3,17 @@ close all;
 clear all;
 % create the nodes
 
-verden = world(10);
+verden = world(0.25);
 
 
-t1 = t_junction(2, 32, 1, 10, 10);
-t2 = left_bend(3, 180, 2, 10, 10);
-t3 = t_junction(4, 90, 3, 10, 10);
+t1 = t_junction(2, 90, 1, 10, 10);
+t2 = left_bend(3, 0, 2, 10, 10);
+t3 = t_junction(4, 0, 3, 10, 10);
 
 
-verden = verden.addNode(t1, [-10;2]);
-verden = verden.addNode(t2, [20; 0]);
-verden = verden.addNode(t3, [20; 20]);
+verden = verden.addNode(t1, [-10;0]);
+verden = verden.addNode(t2, [-10; 10]);
+verden = verden.addNode(t3, [20; 10]);
 
 % t1.draw_edges();
 % t2.draw_edges();
